dc040a297a65fa7da4a017353f76949ec6d6f4cd
brucekomike <brucekomike@gmail.com>
2024-11-09 22:27:37 +0800

    add systemd

--- ssh/client/config.example.sh
+++ ssh/client/config.example.sh
@@ -5,3 +5,19 @@
 #  \___\___/|_| |_|_| |_|\__, |
 #                        |___/                 
 init_system=systemd
+proxyuser=proxyuser
+
+
+
+if [ "init_system" = "systemd" ]; then
+  DAEMON_restart="systemctl restart "
+  DAEMON_reload="systemctl reload "
+  DAEMON_start="systemctl start "
+  DAMMON_stop="systemctl stop "
+fi
+
+
+adduser -m -s /bin/bash $proxyuser
+mkdir /home/$proxyuser/.ssh
+cd /home/$proxyuser/.ssh
+ssh-keygen -t ed25519 -f proxy_key -N ""
